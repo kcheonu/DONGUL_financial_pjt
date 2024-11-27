12800e7f92e28fa5dec95c8d5cb9365bf16ae995
kcheonu <kcheonu@gmail.com>
2024-11-27 09:06:05 +0900

    final_submit

--- psj/dongul_1125/DONGUL_back/exchange/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('load_latest_data/', views.latest_exchange_rates),
-    path('selected_currencies/', views.selected_currencies),
-]
